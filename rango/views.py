4008e01521492f69d957de60c6d1cfc4a1518704
Yike Liu <2904316l@student.gla.ac.uk>
2025-02-14 14:11:57 +0000

    Chapter10 with exercies done

--- rango/views.py
+++ rango/views.py
@@ -15,17 +15,15 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    context_dict[visits] = int(request.COOKIES.get('visits', '1'))
-    response = render(request, 'rango/index.html', context=context_dict)
-    visitor_cookie_handler(request,response)
 
-    return response
+    visitor_cookie_handler(request)
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    if request.session.test_cookie_worked():
-        print("TEST COOKIE WORKED!")
-        request.session.delete_test_cookie()
-    return render(request, 'rango/about.html')
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -141,6 +139,12 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
 
 def visitor_cookie_handler(request):
     visits = int(get_server_side_cookie(request, 'visits', '1'))
